0bd9474e92428004ac4fa082269e4f200589ee65
Daniel Townsend <daniel.townsend@worldspan.co.uk>
2019-07-10 16:32:11 +0100

    added version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 import os
 from setuptools import find_packages, setup
 
-from piccolo_api import __VERSION__ as VERSION
+from piccolo_admin import __VERSION__ as VERSION
 
 
 directory = os.path.abspath(os.path.dirname(__file__))
