b46767acdb2b5ab8362bb3404dc3c3da3b55012e
Daniel Townsend <dan@dantownsend.co.uk>
2019-10-31 21:52:09 +0000

    fleshing out login

--- admin_ui/src/views/Login.vue
+++ admin_ui/src/views/Login.vue
@@ -1,12 +1,12 @@
 <template>
 <div id="login">
     <h1>Login</h1>
-    <form>
+    <form v-on:submit.prevent="login">
         <label>Username</label>
-        <input type="text">
+        <input type="text" v-model="username">
 
         <label>Password</label>
-        <input type="password">
+        <input type="password" v-model="password">
 
         <button>Login</button>
     </form>
@@ -15,8 +15,24 @@
 
 
 <script>
-export default {
+import axios from 'axios'
 
+const BASE_URL = process.env.VUE_APP_BASE_URI
+
+export default {
+    data: function() {
+        return {
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        async login() {
+            console.log("Logging in")
+            let response = await axios.post(`${BASE_URL}login/`, {username: this.username, password: this.password})
+            this.$router.push({name: 'home'})
+        }
+    }
 }
 </script>
 
