a93600bee4adae148f12c632edf284f922229585
Daniel Townsend <dan@dantownsend.co.uk>
2019-11-26 08:29:34 +0000

    using readables in select widgets

--- admin_ui/src/components/KeySelect.vue
+++ admin_ui/src/components/KeySelect.vue
@@ -1,21 +1,21 @@
 <template>
 <select v-bind:name="fieldName">
     <option
-        v-for="_value in ids"
-        v-bind:key="_value"
-        v-bind:value="_value"
-        v-bind:selected="value === _value">{{ _value }}</option>
+        v-for="(readable, id) in ids"
+        v-bind:key="id"
+        v-bind:value="id"
+        v-bind:selected="value == id">{{ readable }}</option>
 </select>
 </template>
 
 
 <script>
 export default {
-    props: {
-        fieldName: String,
-        tableName: String,
-        value: Number
-    },
+    props: [
+        'fieldName',
+        'tableName',
+        'value'
+    ],
     data() {
         return {
             ids: []
