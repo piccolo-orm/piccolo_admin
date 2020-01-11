5cdaafe607ff0a86641116dc7a7d6a74cd0b09a6
Daniel Townsend <dan@dantownsend.co.uk>
2020-01-11 11:47:05 +0000

    changed overlay style to fixed

--- admin_ui/src/components/Modal.vue
+++ admin_ui/src/components/Modal.vue
@@ -22,11 +22,11 @@ export default {}
 
 <style scoped lang="less">
 div#overlay {
-    position: absolute;
-    top: 0%;
-    left: 0%;
-    height: 100%;
-    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
     background-color: rgba(0, 0, 0, 0.7);
 
     div.modal {
