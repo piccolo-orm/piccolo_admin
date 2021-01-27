0bcf744f11711e8000e2658d1b522ea2851731b6
Daniel Townsend <dan@dantownsend.co.uk>
2021-01-27 22:00:19 +0000

    updated example.py to use targ and to support postgres

--- piccolo_admin/sandbox.py
+++ piccolo_admin/sandbox.py
@@ -2,17 +2,18 @@
 Runs the admin in read only mode - useful for letting people evaluate the admin
 online without risk of abuse.
 """
-import sys
-
-import uvicorn
 from piccolo_admin.example import (
     Director,
     Movie,
     Sessions,
     User,
     create_admin,
+    create_schema,
     populate_data,
+    set_engine,
 )
+import targ
+import uvicorn
 
 
 APP = create_admin(
@@ -20,15 +21,25 @@ APP = create_admin(
 )
 
 
-def main(host="localhost", port=8080):
+def run(host="localhost", port=8080):
+    set_engine()
+    create_schema(persist=False)
     populate_data()
     uvicorn.run(APP, host=host, port=port)
 
 
-if __name__ == "__main__":
-    args = sys.argv
+def sandbox(production: bool = False):
+    """
+    Run the Piccolo Admin in read only mode with a SQLite database.
+    """
     kwargs = {}
-    if "--production" in args:
+    if production:
         kwargs.update({"host": "0.0.0.0", "port": 80})
 
-    main(**kwargs)
+    run(**kwargs)
+
+
+if __name__ == "__main__":
+    cli = targ.CLI()
+    cli.register(sandbox)
+    cli.run(solo=True)
