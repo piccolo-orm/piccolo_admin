2ce4965d080b007a249d2d0e19582ca5cca9d1de
sinisaos <sinisaos@gmail.com>
2021-02-22 09:05:53 +0100

    Improve foreign key selector

--- admin_ui/src/components/KeySelect.vue
+++ admin_ui/src/components/KeySelect.vue
@@ -1,26 +1,33 @@
 <template>
-    <select
-        v-bind:name="fieldName"
-        v-model="selectedValue"
-        v-on:change="$emit('valueChanged', selectedValue)"
-    >
-        <option
-            v-if="isFilter"
-            value="all"
-        >All</option>
-        <option
-            v-if="isNullable"
-            value="null"
-        >Null</option>
-        <option
-            :key="id"
-            :selected="value == id"
-            :value="id"
-            v-for="(readable, id) in ids"
-        >{{ readable }}</option>
-    </select>
+    <div>
+        <input
+            :placeholder="'Search ' + tableName + 's'"
+            autocomplete="off"
+            type="text"
+            v-bind:name="fieldName"
+            v-model="selectedValue"
+            v-on:focus="resetResult(), toggle = true"
+            v-on:keydown.enter.prevent
+        />
+        <div
+            id="results"
+            v-if="toggle"
+        >
+            <div
+                :key="id"
+                v-for="(readable, id) in dropdownItems"
+                v-on:click="selectResult(readable, id), toggle = false"
+                v-on:focus="resetResult(), toggle = true"
+            >{{ readable }}</div>
+        </div>
+        <input
+            :value="hiddenSelectedValue"
+            type="hidden"
+            v-bind:name="fieldName"
+            v-on:focus="toggle = true"
+        />
+    </div>
 </template>
-
 
 <script>
 export default {
@@ -28,19 +35,14 @@ export default {
         fieldName: String,
         tableName: String,
         value: undefined,
-        isFilter: {
-            type: Boolean,
-            default: false
-        },
-        isNullable: {
-            type: Boolean,
-            default: false
-        }
     },
     data() {
         return {
             ids: [],
-            selectedValue: undefined
+            dropdownItems: {},
+            selectedValue: undefined,
+            hiddenSelectedValue: undefined,
+            toggle: false,
         }
     },
     methods: {
@@ -50,23 +52,63 @@ export default {
                 this.tableName
             )
             this.ids = response.data
-        }
+        },
+        selectResult(readable, id) {
+            this.selectedValue = readable
+            this.hiddenSelectedValue = id
+        },
+        // need to explicit clear values
+        resetResult() {
+            this.selectedValue = undefined
+            this.hiddenSelectedValue = undefined
+        },
     },
     watch: {
         async tableName() {
             await this.fetchData()
         },
-        value() {
-            this.selectedValue = this.value
-        }
+        selectedValue(searchValue) {
+            this.dropdownItems = {}
+            for (const property in this.ids) {
+                if (
+                    this.ids[property]
+                        .toLowerCase()
+                        .includes(searchValue.toLowerCase())
+                ) {
+                    this.dropdownItems[property] = this.ids[property]
+                }
+            }
+        },
     },
     async mounted() {
-        await this.fetchData()
         this.selectedValue = this.value
-    }
+        this.hiddenSelectedValue = this.value
+        await this.fetchData()
+    },
 }
 </script>
 
 
-<style>
+<style lang="less" scoped>
+@import "../vars.less";
+
+.light_mode {
+    background-color: white;
+    color: @dark_grey;
+    #results {
+        cursor: pointer;
+        background-color: darken(white, 5%);
+        color: @dark_grey;
+    }
+}
+
+.dark_mode {
+    background-color: @dark_grey;
+    color: @off_white;
+    #results {
+        cursor: pointer;
+        background-color: darken(@dark_grey, 4%);
+        color: @off_white;
+    }
+}
 </style>
--- admin_ui/src/components/RowForm.vue
+++ admin_ui/src/components/RowForm.vue
@@ -32,12 +32,9 @@
                 </label>
                 <KeySelect
                     v-bind:fieldName="property.title.toLowerCase()"
-                    v-bind:isFilter="isFilter"
-                    v-bind:isNullable="property.nullable"
-                    v-bind:key="getKey(keyName)"
+                    v-bind:key="getValue(property.title)"
                     v-bind:tableName="property.extra.to"
                     v-bind:value="getValue(property.title)"
-                    v-on:valueChanged="keySelectChanged(property.title, $event)"
                 />
             </template>
             <template v-else>
@@ -75,68 +72,35 @@ export default Vue.extend({
         schema: Object,
         isFilter: {
             type: Boolean,
-            default: false
-        }
+            default: false,
+        },
     },
     components: {
         InputField,
-        KeySelect
+        KeySelect,
     },
     data() {
         return {
             keySelectIDs: {},
-            baseIndex: 1,
-            windowListener: undefined
         }
     },
     methods: {
         getValue(propertyTitle: string) {
             let value = this.row
-                ? this.row[
-                      propertyTitle
-                          .toLowerCase()
-                          .split(" ")
-                          .join("_")
-                  ]
+                ? this.row[propertyTitle.toLowerCase().split(" ").join("_")]
                 : undefined
             return value
         },
         getKeySelectID(propertyTitle: string) {
-            return (
-                this.keySelectIDs[propertyTitle] || this.getValue(propertyTitle)
-            )
-        },
-        keySelectChanged(propertyTitle: string, value: number) {
-            console.log(`${propertyTitle} = ${value}`)
-            Vue.set(this.keySelectIDs, propertyTitle, value)
-        },
-        // We use this to refresh KeySelect components
-        getKey(keyName: string) {
-            return keyName + this.baseIndex
+            return this.getValue(propertyTitle)
         },
         isRequired(keyName: string) {
             return (
                 !this.isFilter &&
                 (this.schema.required || []).indexOf(keyName) != -1
             )
-        }
+        },
     },
-    mounted() {
-        window.addEventListener("message", receiveMessage, false)
-
-        let app = this
-
-        function receiveMessage(event) {
-            if (event.origin !== document.location.origin) return
-            console.log("Received message")
-            app.baseIndex = app.baseIndex + 1
-        }
-    },
-    destroyed() {
-        if (this.windowListener) {
-            window.removeEventListener("message", this.windowListener)
-        }
-    }
 })
 </script>
 
