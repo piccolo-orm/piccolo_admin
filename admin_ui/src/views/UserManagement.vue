ff15de8e90ad79ad6bdec57fbf5568e0ea17fd11
Daniel Townsend <dan@dantownsend.co.uk>
2021-05-08 10:01:15 +0100

    only allow superusers to change other user's passwords

--- admin_ui/src/views/UserManagement.vue
+++ admin_ui/src/views/UserManagement.vue
@@ -1,46 +1,59 @@
 <template>
     <BaseView>
-        <PasswordModal
-            v-if="showPasswordModal"
-            v-on:close="showPasswordModal = false"
-            v-bind:username="activeUser.username"
-        />
+        <template v-if="activeUser && userList">
+            <PasswordModal
+                v-if="showPasswordModal"
+                v-on:close="showPasswordModal = false"
+                v-bind:username="activeUser.username"
+            />
 
-        <div class="users_lists">
-            <TitleBar title="Users"></TitleBar>
-            <Table>
-                <template v-slot:thead>
-                    <tr>
-                        <th>Username</th>
-                        <th>Active</th>
-                        <th>Admin</th>
-                        <th>Superuser</th>
-                        <th>Tools</th>
-                    </tr>
-                </template>
+            <div class="users_lists">
+                <TitleBar title="Users"></TitleBar>
+                <Table>
+                    <template v-slot:thead>
+                        <tr>
+                            <th>Username</th>
+                            <th>Active</th>
+                            <th>Admin</th>
+                            <th>Superuser</th>
+                            <th>Tools</th>
+                        </tr>
+                    </template>
 
-                <template v-slot:tbody>
-                    <tr v-for="(user, index) in userList" :key="index">
-                        <td>{{ user.username }}</td>
-                        <td><BooleanIcon v-bind:boolean="user.active" /></td>
-                        <td><BooleanIcon v-bind:boolean="user.admin" /></td>
-                        <td><BooleanIcon v-bind:boolean="user.superuser" /></td>
-                        <td>
-                            <span style="display: block; text-align: right">
-                                <a
-                                    href="#"
-                                    v-on:click.prevent="
-                                        activeUser = user
-                                        showPasswordModal = true
+                    <template v-slot:tbody>
+                        <tr v-for="(user, index) in userList" :key="index">
+                            <td>{{ user.username }}</td>
+                            <td>
+                                <BooleanIcon v-bind:boolean="user.active" />
+                            </td>
+                            <td><BooleanIcon v-bind:boolean="user.admin" /></td>
+                            <td>
+                                <BooleanIcon v-bind:boolean="user.superuser" />
+                            </td>
+                            <td>
+                                <span
+                                    style="display: block; text-align: right"
+                                    v-if="
+                                        activeUser.superuser ||
+                                        activeUser.username == user.username
                                     "
-                                    >Change password</a
                                 >
-                            </span>
-                        </td>
-                    </tr>
-                </template>
-            </Table>
-        </div>
+                                    <a
+                                        href="#"
+                                        v-on:click.prevent="
+                                            activeUser = user
+                                            showPasswordModal = true
+                                        "
+                                        >Change password</a
+                                    >
+                                </span>
+                            </td>
+                        </tr>
+                    </template>
+                </Table>
+            </div>
+        </template>
+        <p v-else>Loading ...</p>
     </BaseView>
 </template>
 
@@ -66,10 +79,12 @@ export default Vue.extend({
     data() {
         return {
             showPasswordModal: false,
-            activeUser: undefined,
         }
     },
     computed: {
+        activeUser(): User {
+            return this.$store.state.user
+        },
         userList(): User[] {
             return this.$store.state.userManagement.userList
         },
