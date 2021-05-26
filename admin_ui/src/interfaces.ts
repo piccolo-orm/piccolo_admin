82cc2b9ca3c1cb285f1b60f01d63309b475c230e
Daniel Townsend <dan@dantownsend.co.uk>
2021-05-26 22:19:48 +0100

    showing the display value for choices in the row listing

--- admin_ui/src/interfaces.ts
+++ admin_ui/src/interfaces.ts
@@ -68,3 +68,35 @@ export interface Choice {
 export interface Choices {
     [key: string]: Choice
 }
+
+/*****************************************************************************/
+
+export interface Schema {
+    title: string;
+    type: string;
+    properties: Properties;
+    help_text: null;
+}
+
+export interface Properties {
+    [key: string]: RowConfig;
+}
+
+export interface RowConfig {
+    title: string;
+    extra: RowConfigExtra;
+    nullable: boolean;
+    type: string;
+    format?: string;
+    maxLength?: number;
+    items?: ArrayItems;
+}
+
+export interface RowConfigExtra {
+    help_text: null | string;
+    choices: Choices | null;
+}
+
+export interface ArrayItems {
+    type: string;
+}
