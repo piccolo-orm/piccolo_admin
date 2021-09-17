c60f1e7f01d7a83806ce5e28651927dbf3e62663
Daniel Townsend <dan@dantownsend.co.uk>
2021-09-17 22:47:37 +0100

    increase test coverage

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,11 +1,13 @@
 from unittest import TestCase
 
+from piccolo.apps.user.tables import BaseUser
 from piccolo.columns.column_types import ForeignKey, Varchar
 from piccolo.table import Table
+from piccolo_api.session_auth.tables import SessionsBase
 from starlette.testclient import TestClient
 
 from piccolo_admin.endpoints import get_all_tables
-from piccolo_admin.sandbox import APP
+from piccolo_admin.example import APP
 from piccolo_admin.version import __VERSION__
 
 
@@ -53,3 +55,66 @@ class TestAdminRouter(TestCase):
         response = client.get("/api/user/")
         self.assertEqual(response.json(), {"error": "Auth failed"})
         self.assertEqual(response.status_code, 401)
+
+
+class TestForms(TestCase):
+    credentials = {"username": "Bob", "password": "bob123"}
+
+    def setUp(self):
+        SessionsBase.create_table(if_not_exists=True).run_sync()
+        BaseUser.create_table(if_not_exists=True).run_sync()
+        BaseUser(
+            **self.credentials, active=True, admin=True, superuser=True
+        ).save().run_sync()
+
+    def tearDown(self):
+        SessionsBase.alter().drop_table().run_sync()
+        BaseUser.alter().drop_table().run_sync()
+
+    def test_forms(self):
+        """
+        Make sure the form listing can be retrieved, and the schema for a form.
+        """
+        client = TestClient(APP)
+
+        # To get a CSRF cookie
+        response = client.get("/")
+        csrftoken = response.cookies["csrftoken"]
+
+        # Login
+        payload = dict(csrftoken=csrftoken, **self.credentials)
+        client.post(
+            "/auth/login/", json=payload, headers={"X-CSRFToken": csrftoken},
+        )
+
+        #######################################################################
+        # List all forms
+
+        response = client.get("/api/forms/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(),
+            [
+                {"name": "Business email form", "slug": "business-email-form"},
+                {"name": "Friends email form", "slug": "friends-email-form"},
+            ],
+        )
+
+        #######################################################################
+        # Now get the schema for a form
+
+        response = client.get("/api/forms/business-email-form/schema/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(),
+            {
+                "title": "BusinessEmailModel",
+                "type": "object",
+                "properties": {
+                    "email": {"title": "Email", "type": "string"},
+                    "title": {"title": "Title", "type": "string"},
+                    "content": {"title": "Content", "type": "string"},
+                },
+                "required": ["email", "title", "content"],
+            },
+        )
