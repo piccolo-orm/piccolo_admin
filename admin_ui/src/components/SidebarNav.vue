4420d1349571b68dd8d044e32a40ed175b48e1c7
sinisaos <sinisaos@gmail.com>
2021-10-28 07:24:51 +0200

    added collapsible tables and forms

--- admin_ui/src/components/SidebarNav.vue
+++ admin_ui/src/components/SidebarNav.vue
@@ -1,24 +1,52 @@
 <template>
     <div class="sidebar">
         <p class="opaque">
-            <router-link class="subtle" to="/">
+            <router-link
+                class="subtle"
+                to="/"
+            >
                 <font-awesome-icon icon="home" />Home
             </router-link>
         </p>
-        <p class="opaque">
+        <p
+            class="opaque"
+            v-on:click="isHiddenTables = !isHiddenTables"
+        >
             <font-awesome-icon icon="table" />Tables
             <span style="float: right">
-                <font-awesome-icon icon="angle-down" />
+                <font-awesome-icon
+                    icon="angle-down"
+                    title="Show tables"
+                    v-if="isHiddenTables"
+                />
+                <font-awesome-icon
+                    icon="angle-up"
+                    title="Hide tables"
+                    v-else
+                />
             </span>
         </p>
-        <TableNav />
-        <p class="opaque" v-show="formConfigs.length > 0">
+        <TableNav v-if="!isHiddenTables" />
+        <p
+            class="opaque"
+            v-if="formConfigs.length > 0"
+            v-on:click="isHiddenForms = !isHiddenForms"
+        >
             <font-awesome-icon icon="cogs" />Forms
             <span style="float: right">
-                <font-awesome-icon icon="angle-down" />
+                <font-awesome-icon
+                    icon="angle-down"
+                    title="Show forms"
+                    v-if="isHiddenForms"
+                />
+                <font-awesome-icon
+                    icon="angle-up"
+                    title="Hide forms"
+                    v-else
+                />
             </span>
         </p>
-        <FormNav />
+        <FormNav v-if="!isHiddenForms" />
     </div>
 </template>
 
@@ -28,6 +56,12 @@ import TableNav from "./TableNav.vue"
 import FormNav from "./FormNav.vue"
 
 export default Vue.extend({
+    data() {
+        return {
+            isHiddenTables: false,
+            isHiddenForms: false,
+        }
+    },
     components: {
         TableNav,
         FormNav,
@@ -50,6 +84,7 @@ div.sidebar {
     p {
         padding: 0.5rem;
         margin: 0;
+        cursor: pointer;
 
         a {
             text-decoration: none;
@@ -61,7 +96,7 @@ div.sidebar {
 
         li {
             a {
-                display: block;
+                display: flex;
                 padding: 0.5rem;
                 text-decoration: none;
                 border-left: 3px solid rgba(0, 0, 0, 0);
