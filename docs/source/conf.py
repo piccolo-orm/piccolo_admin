9b492009aef9e624529c3bc2579de385b1c18a36
Daniel Townsend <dan@dantownsend.co.uk>
2021-11-04 10:50:22 +0000

    add logo to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,12 +43,6 @@ extensions = ["sphinx.ext.autodoc", "sphinx_autodoc_typehints"]
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -60,3 +54,6 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+html_logo = "logo.png"
+html_theme_options = {"logo_only": True}
