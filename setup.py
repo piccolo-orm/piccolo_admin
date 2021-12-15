3d20fe20fc009d0c40464734b48ee5739df7cfbf
Daniel Townsend <dan@dantownsend.co.uk>
2021-12-15 09:55:18 +0000

    add new supported Python versions to setup.py

--- setup.py
+++ setup.py
@@ -43,6 +43,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Database :: Front-Ends",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Content Management System"  # noqa: E501
