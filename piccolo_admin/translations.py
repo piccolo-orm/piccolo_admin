9497d59d7f671d7791325dba34414ecf3586f065
Daniel Townsend <dan@dantownsend.co.uk>
2022-07-04 14:44:42 +0100

    add example translation

---
+++ piccolo_admin/translations.py
@@ -0,0 +1,27 @@
+WELSH = {
+    "About": None,
+    "Add Row": None,
+    "Apply": None,
+    "Back": "ol",
+    "Clear filters": None,
+    "Close": "Cau",
+    "Dark Mode": "Modd Yywyll",
+    "Delete": "Dileu",
+    "Export CSV": None,
+    "Filter": "Ffilter",
+    "Hide referencing tables": None,
+    "Home": "Cartref",
+    "Language": "Iaith",
+    "Light Mode": "Modd Golau",
+    "Log out": "Allgofnodi",
+    "Per page": None,
+    "Save": "Arbed",
+    "Select one of the tables in the sidebar to get started.": None,
+    "Show filters": None,
+    "Show referencing tables": None,
+    "Sort": "Didoli",
+    "Tables": "Tablau",
+}
+
+
+TRANSLATIONS = [WELSH]
