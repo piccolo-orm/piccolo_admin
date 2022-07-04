fb946a1ba865a3ab6247e29b5fe454b3419e9d53
Daniel Townsend <dan@dantownsend.co.uk>
2022-07-04 15:49:21 +0100

    add example translation (#174)
    * add example translation
    
    * add rest of translations
    
    * add comment about why translations.py exists

---
+++ piccolo_admin/translations.py
@@ -0,0 +1,31 @@
+# See https://github.com/piccolo-orm/piccolo_admin/issues/173 for more info.
+
+WELSH = {
+    "About": "Am",
+    "Add Row": "Ychwanegu Rhes",
+    "Apply": "Ymgeisiwch",
+    "Back": "Ol",
+    "Clear filters": "Clirio hidlwyr",
+    "Close": "Cau",
+    "Dark Mode": "Modd Yywyll",
+    "Delete": "Dileu",
+    "Export CSV": "Allforio CSV",
+    "Filter": "Ffilter",
+    "Hide referencing tables": "Cuddio tablau cyfeirio",
+    "Home": "Cartref",
+    "Language": "Iaith",
+    "Light Mode": "Modd Golau",
+    "Log out": "Allgofnodi",
+    "Per page": "Fesul tudalen",
+    "Save": "Arbed",
+    "Select one of the tables in the sidebar to get started.": (
+        "Dewiswch un o'r tablau yn y bar ochr i ddechrau."
+    ),
+    "Show filters": "Dangos hidlwyr",
+    "Show referencing tables": "Dangos tablau cyfeirio",
+    "Sort": "Didoli",
+    "Tables": "Tablau",
+}
+
+
+TRANSLATIONS = [WELSH]
