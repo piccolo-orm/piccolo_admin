31516f17e5507143e0aa1e055ff5f98e1d85bcef
Daniel Townsend <dan@dantownsend.co.uk>
2022-07-08 08:24:05 +0100

    added support for foreign keys and nullable columns in bulk update modal

--- admin_ui/src/components/BulkUpdateModal.vue
+++ admin_ui/src/components/BulkUpdateModal.vue
@@ -1,40 +1,60 @@
 <template>
-    <Modal v-on:close="$emit('close')">
+    <Modal v-on:close="$emit('close')" title="Bulk Update">
         <template v-if="schema">
-            <p>Select a column to update</p>
+            <p>Select a column to update:</p>
 
-            <select name="property" v-model="propertyName">
-                <option
-                    :key="key"
-                    :value="key"
-                    v-for="(property, key) in schema.properties"
-                >
-                    {{ key }}
-                </option>
+            <select name="property" v-model="selectedPropertyName">
+                <option value="null" disabled>Select a Column</option>
+                <template v-for="(property, key) in schema.properties">
+                    <option
+                        :key="key"
+                        :value="key"
+                        v-if="key != schema.primary_key_name"
+                    >
+                        {{ property.title }}
+                    </option>
+                </template>
             </select>
-            <p v-show="propertyName">Value to change</p>
 
-            <form v-on:submit.prevent="updateRows($event)">
-                <div :key="key" v-for="(property, key) in schema.properties">
-                    <InputField
-                        v-if="key == propertyName"
-                        v-bind:format="property.format"
-                        v-bind:isFilter="false"
-                        v-bind:key="property.title"
-                        v-bind:title="property.title"
-                        v-bind:type="property.type || property.anyOf[0].type"
-                        v-bind:choices="property.extra.choices"
-                    />
+            <p v-show="selectedPropertyName">New value:</p>
+
+            <form
+                v-on:submit.prevent="updateRows($event)"
+                v-if="selectedPropertyName"
+            >
+                <KeySearch
+                    v-bind:fieldName="selectedPropertyName"
+                    v-bind:key="selectedProperty.title"
+                    v-bind:tableName="selectedProperty.extra.to"
+                    v-bind:isNullable="selectedProperty.nullable"
+                    v-if="selectedProperty.extra.foreign_key"
+                />
+
+                <InputField
+                    v-bind:format="selectedProperty.format"
+                    v-bind:isFilter="false"
+                    v-bind:title="selectedProperty.title"
+                    v-bind:type="
+                        selectedProperty.type || selectedProperty.anyOf[0].type
+                    "
+                    v-bind:choices="selectedProperty.extra.choices"
+                    v-else
+                />
+
+                <div class="set_null" v-if="selectedProperty.nullable">
+                    <label>Set null:</label>
+                    <input type="checkbox" v-model="nullCheckboxState" />
                 </div>
+
                 <button>Update</button>
             </form>
         </template>
     </Modal>
 </template>
 
-
 <script lang="ts">
 import InputField from "../components/InputField.vue"
+import KeySearch from "../components/KeySearch.vue"
 import Modal from "../components/Modal.vue"
 import * as i from "../interfaces"
 
@@ -49,11 +69,18 @@ export default {
     },
     components: {
         Modal,
-        InputField
+        InputField,
+        KeySearch
     },
     data() {
         return {
-            propertyName: null
+            selectedPropertyName: null,
+            nullCheckboxState: false
+        }
+    },
+    computed: {
+        selectedProperty() {
+            return this.schema.properties[this.selectedPropertyName]
         }
     },
     methods: {
@@ -63,58 +90,63 @@ export default {
             const form = new FormData(event.target)
 
             const json = {}
-            for (const i of form.entries()) {
-                const key = i[0].split(" ").join("_")
-                let value = i[1]
 
-                if (value == "null") {
-                    value = null
-                } else if (this.schema.properties[key].type == "array") {
-                    // @ts-ignore
+            let value = null
+
+            if (!this.nullCheckboxState) {
+                value = form.get(this.selectedPropertyName)
+
+                if (this.selectedProperty.type == "array") {
                     value = JSON.parse(value)
-                } else if (
-                    this.schema?.properties[key].format == "date-time" &&
-                    value == ""
-                ) {
-                    value = null
-                } else if (
-                    this.schema?.properties[key].extra.foreign_key == true &&
-                    value == ""
-                ) {
-                    value = null
                 }
-                json[key] = value
             }
 
-            for (let i = 0; i < this.selectedRows.length; i++) {
-                await this.$store
-                    .dispatch("updateRow", {
+            json[this.selectedPropertyName] = value
+
+            try {
+                // TODO - we will use the new bulk update endpoint once it is
+                // merged into PiccoloCRUD.
+                for (let i = 0; i < this.selectedRows.length; i++) {
+                    await this.$store.dispatch("updateRow", {
                         tableName: this.tableName,
                         rowID: this.selectedRows[i],
                         data: json
                     })
-                    .then(() => {
-                        var message: i.APIResponseMessage = {
-                            contents: "Successfully updated rows",
-                            type: "success"
-                        }
-                        this.$store.commit("updateApiResponseMessage", message)
-                    })
-                    .catch((error) => {
-                        console.log(error)
-                        var message: i.APIResponseMessage = {
-                            contents: "Invalid column name or value",
-                            type: "error"
-                        }
-                        this.$store.commit("updateApiResponseMessage", message)
-                    })
+                }
+                var message: i.APIResponseMessage = {
+                    contents: "Successfully updated rows",
+                    type: "success"
+                }
+                this.$store.commit("updateApiResponseMessage", message)
+
+                this.$emit("close")
+
+                await this.$store.dispatch("fetchRows")
+            } catch (error) {
+                console.log(error)
+                var message: i.APIResponseMessage = {
+                    contents: "Invalid column name or value",
+                    type: "error"
+                }
+                this.$store.commit("updateApiResponseMessage", message)
             }
-            this.$emit("close")
-            await this.fetchRows()
-        },
-        async fetchRows() {
-            await this.$store.dispatch("fetchRows")
         }
     }
 }
-</script> 
+</script>
+
+<style lang="less">
+div.set_null {
+    display: flex;
+    flex-direction: row;
+    align-items: baseline;
+
+    label {
+        flex-grow: 1;
+    }
+
+    input {
+        width: auto !important;
+    }
+}
+</style>
