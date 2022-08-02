e784d487415b01d903555df5dc3eafabd29fc617
Daniel Townsend <dan@dantownsend.co.uk>
2022-08-02 11:02:50 +0100

    added tests for S3

---
+++ tests/media/test_base.py
@@ -0,0 +1,75 @@
+import tempfile
+import uuid
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+
+from piccolo_admin.media.local import LocalMediaStorage
+
+
+class TestGenerateFileID(TestCase):
+    def setUp(self) -> None:
+        self.media_path = tempfile.gettempdir()
+        self.storage = LocalMediaStorage(media_path=self.media_path)
+
+    def test_starts_with_period(self):
+        with self.assertRaises(ValueError) as manager:
+            self.storage.generate_file_id(file_name=".private_file.jpeg")
+
+        self.assertEqual(
+            str(manager.exception), "File names must not start with a period."
+        )
+
+    def test_double_period(self):
+        """
+        A file_name containing a double period shouldn't be allowed, as it
+        could potentially be used to traverse the file system.
+        """
+        with self.assertRaises(ValueError) as manager:
+            self.storage.generate_file_id(file_name="test..file.jpeg")
+
+        self.assertEqual(
+            str(manager.exception), "File names must not contain '..'."
+        )
+
+    def test_empty_file_name(self):
+        with self.assertRaises(ValueError) as manager:
+            self.storage.generate_file_id(file_name="")
+
+        self.assertEqual(
+            str(manager.exception), "The file name can't be empty."
+        )
+
+    def test_allowed_extensions(self):
+        with self.assertRaises(ValueError) as manager:
+            self.storage.generate_file_id(
+                file_name="test.abcdefghijklmonpqrstuvwxyz123"
+            )
+
+        self.assertEqual(
+            str(manager.exception), "This file type isn't allowed."
+        )
+
+    def test_allowed_characters(self):
+        with self.assertRaises(ValueError) as manager:
+            self.storage.generate_file_id(file_name="@{£}%^*jpeg")
+
+        self.assertEqual(
+            str(manager.exception), "'@' is not allowed in the filename."
+        )
+
+    @patch("piccolo_admin.media.base.uuid")
+    def test_long_file_name(self, uuid_module: MagicMock):
+        """
+        Make sure that really long file names are truncated.
+        """
+        uuid_module.uuid4.return_value = uuid.UUID(
+            "fd0125c7-8777-4976-83c1-81605d5ab155"
+        )
+
+        truncated_file_id = self.storage.generate_file_id(
+            file_name="".join("a" for _ in range(200)) + ".jpg"
+        )
+        self.assertEqual(
+            truncated_file_id,
+            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa-fd0125c7-8777-4976-83c1-81605d5ab155.jpg",  # noqa: E501
+        )
---
+++ tests/media/test_s3.py
@@ -0,0 +1,68 @@
+import os
+import uuid
+from unittest import TestCase
+from unittest.mock import MagicMock, patch
+
+import boto3
+from moto import mock_s3
+
+from piccolo_admin.media.s3 import S3MediaStorage
+
+
+class TestS3MediaStorage(TestCase):
+    @patch("piccolo_admin.media.base.uuid")
+    @patch("piccolo_admin.media.s3.S3MediaStorage.get_client")
+    def test_store_file(self, get_client: MagicMock, uuid_module: MagicMock):
+        """
+        Make sure we can store files, and retrieve them.
+        """
+        uuid_module.uuid4.return_value = uuid.UUID(
+            "fd0125c7-8777-4976-83c1-81605d5ab155"
+        )
+
+        with mock_s3():
+            s3 = boto3.resource("s3", region_name="us-east-1")
+            s3.create_bucket(Bucket="posters")
+
+            connection_kwargs = {
+                "aws_access_key_id": "abc123",
+                "aws_secret_access_key": "xyz123",
+                "region_name": "us-east-1",
+            }
+
+            get_client.return_value = boto3.client("s3", **connection_kwargs)
+
+            storage = S3MediaStorage(
+                bucket_name="posters", connection_kwargs=connection_kwargs
+            )
+
+            with open(
+                os.path.join(os.path.dirname(__file__), "test_files/bulb.jpg"),
+                "rb",
+            ) as test_file:
+                # Store the file
+                file_id = storage.store_file_sync(
+                    file_name="bulb.jpg", file=test_file
+                )
+
+                # Retrieve the URL for the file
+                url = storage.generate_file_url_sync(file_id, root_url="")
+
+                path, params = url.split("?", 1)
+
+                self.assertEqual(
+                    path, f"https://posters.s3.amazonaws.com/{file_id}"
+                )
+
+                # We're parsing a string like this:
+                # AWSAccessKeyId=abc123&Signature=abc123&Expires=1659437428
+                params_list = [i.split("=") for i in params.split("&")]
+
+                params_dict = {i[0]: i[1] for i in params_list}
+
+                self.assertEqual(
+                    params_dict["AWSAccessKeyId"],
+                    connection_kwargs["aws_access_key_id"],
+                )
+                self.assertTrue("Signature" in params_dict)
+                self.assertTrue("Expires" in params_dict)
