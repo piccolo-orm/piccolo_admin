bd426e7afd085ab090b8d1a03f11d6c2c63ceb97
Daniel Townsend <dan@dantownsend.co.uk>
2022-08-02 19:41:09 +0100

    added test for `LocalMediaStorage.bulk_delete`

--- tests/media/test_local.py
+++ tests/media/test_local.py
@@ -48,28 +48,56 @@ class TestLocalMediaStorage(TestCase):
             "rb",
         ) as test_file:
             # Store the file
-            file_id = storage.store_file_sync(
+            file_key = storage.store_file_sync(
                 file_name="bulb.jpg", file=test_file
             )
 
             # Make sure the file was stored.
-            self.assertTrue(file_id in os.listdir(media_path))
+            self.assertTrue(file_key in os.listdir(media_path))
 
             # Make sure the permissions are correct
             self.assertEqual(
-                oct(os.stat(os.path.join(media_path, file_id)).st_mode)[-3:],
+                oct(os.stat(os.path.join(media_path, file_key)).st_mode)[-3:],
                 "640",
             )
 
             # Retrieve the URL for the file
-            url = storage.generate_file_url_sync(file_id, root_url="/media/")
+            url = storage.generate_file_url_sync(file_key, root_url="/media/")
             self.assertEqual(
                 url, "/media/bulb-fd0125c7-8777-4976-83c1-81605d5ab155.jpg"
             )
 
             # Retrieve the file itself
-            file = asyncio.run(storage.get_file(file_key=file_id))
+            file = asyncio.run(storage.get_file(file_key=file_key))
             assert file is not None
             test_file.seek(0, 0)
             self.assertEqual(file.read(), test_file.read())
             file.close()
+
+            # List all of the files
+            file_list = asyncio.run(storage.get_file_keys())
+            self.assertEqual(file_list, [file_key])
+
+            # Delete the file
+            asyncio.run(storage.delete_file(file_key=file_key))
+            self.assertEqual(os.listdir(storage.media_path), [])
+
+    def test_bulk_delete(self):
+        media_path = os.path.join(tempfile.gettempdir(), "piccolo-admin-media")
+
+        if os.path.exists(media_path):
+            shutil.rmtree(media_path)
+
+        os.mkdir(media_path)
+
+        for file_name in ("file_1.txt", "file_2.txt", "file_3.txt"):
+            with open(os.path.join(media_path, file_name), "w") as f:
+                f.write("test")
+
+        storage = LocalMediaStorage(column=Movie.poster, media_path=media_path)
+
+        asyncio.run(
+            storage.bulk_delete_files(file_keys=["file_1.txt", "file_2.txt"])
+        )
+
+        self.assertListEqual(os.listdir(media_path), ["file_3.txt"])
