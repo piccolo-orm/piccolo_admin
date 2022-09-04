20ffe2fd615d441ed7cc84445e81e0393a461198
Daniel Townsend <dan@dantownsend.co.uk>
2022-09-04 17:39:48 +0100

    try running Python and Vue servers in same step

---
+++ scripts/dev-server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+# Get the absolute path for the root folder, so we can call this script from
+# anywhere and it will work.
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && pwd)"
+
+set -x
+
+(cd $ROOT_DIR && python -m piccolo_admin.example) & (cd $ROOT_DIR/admin_ui && npm run serve)
--- scripts/start_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-set -x
-
-export PYTHONPATH='..':$PYTHONPATH
-python -m piccolo_admin.example &
