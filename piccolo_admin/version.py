3d88c47f57cb4295b1625dc65d95ab6cda53f750
Daniel Townsend <dan@dantownsend.co.uk>
2022-09-14 21:19:54 +0100

    move version number into version.txt
    This means that we can read the version number from `setup.py` without having to import any code from piccolo_admin.

--- piccolo_admin/version.py
+++ piccolo_admin/version.py
@@ -1,1 +1,9 @@
-__VERSION__ = "0.33.0"
+import os
+
+
+def get_version():
+    with open(os.path.join(os.path.dirname(__file__), "version.txt")) as f:
+        return f.read().strip()
+
+
+__VERSION__ = get_version()
