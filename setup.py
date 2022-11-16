483b40e64c64b4078dda60f1c068cc8b142290ab
Daniel Townsend <dan@dantownsend.co.uk>
2022-11-16 17:07:37 +0000

    add Python 3.11 to setup.py

--- setup.py
+++ setup.py
@@ -86,6 +86,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Database :: Front-Ends",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Content Management System",  # noqa: E501
