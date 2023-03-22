15fb3865e5c6cdc47d9c17cf027704bf6b19b2e5
Daniel Townsend <dan@dantownsend.co.uk>
2023-03-22 16:17:18 +0000

    playwright fixes

--- e2e/pages.py
+++ e2e/pages.py
@@ -26,18 +26,54 @@ class LoginPage:
     def login(self):
         self.username_input.fill(USERNAME)
         self.password_input.fill(PASSWORD)
-        self.login_button.click()
+
+        with self.page.expect_response(
+            lambda response: response.url == f"{BASE_URL}/public/login/"
+            and response.request.method == "POST"
+            and response.status == 200
+        ):
+            self.login_button.click()
+
+        self.page.wait_for_url(f"{BASE_URL}/#/")
 
 
-class NullableColumnsTableAdd:
-    url = f"{BASE_URL}/#/nullable_columns/add"
-
-    def __init__(self, page: Page):
+class RowListingPage:
+    def __init__(self, page: Page, tablename: str):
         self.page = page
+        self.url = f"{BASE_URL}/#/{tablename}"
+        self.sort_button = page.locator("a[data-uitest=sort_button]")
+        self.sort_by_selector = page.locator(
+            "select[data-uitest=sort_by_selector]"
+        )
+
+    def reset(self):
+        self.page.goto(self.url)
+
+    def open_sort_modal(self):
+        self.sort_button.click()
+
+    def get_sort_by_column(self) -> str:
+        """
+        Returns the name of the column being sorted by.
+        """
+        return self.sort_by_selector.input_value()
+
+
+class AddRowPage:
+    def __init__(self, page: Page, tablename: str):
+        self.page = page
+        self.tablename = tablename
+        self.url = f"{BASE_URL}/#/{tablename}/add"
         self.create_button = page.locator("button[data-uitest=create_button]")
 
     def reset(self):
         self.page.goto(self.url)
 
     def submit_form(self):
-        self.create_button.click()
+        with self.page.expect_response(
+            lambda response: response.url
+            == f"{BASE_URL}/api/tables/{self.tablename}/"
+            and response.request.method == "POST"
+            and response.status == 201
+        ):
+            self.create_button.click()
