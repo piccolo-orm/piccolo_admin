3361cd46fdca3c1728e6897733f7c4ae61b3ba60
sinisaos <sinisaos@gmail.com>
2023-05-24 11:41:52 +0200

    add custom links option in sidebar nav (#308)
    * add custom links option in sidebar nav
    
    * fix missing translation
    
    * replaced list of tuples with dict
    
    * add docs
    
    * rearrange docs slightly
    
    * use older flake8
    
    * add back `exact`
    
    I get an error when doing this locally - must have an older version of playwright installed.
    
    ---------
    
    Co-authored-by: Daniel Townsend <dan@dantownsend.co.uk>

--- piccolo_admin/example.py
+++ piccolo_admin/example.py
@@ -440,6 +440,10 @@ APP = create_admin(
     ],
     auth_table=User,
     session_table=Sessions,
+    sidebar_links={
+        "Top Movies": "/#/movie?__order=-box_office",
+        "Google": "https://google.com",
+    },
 )
 
 
