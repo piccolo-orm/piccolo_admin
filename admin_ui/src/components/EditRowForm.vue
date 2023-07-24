2b1e3e0ade37fce4f3ac8f0fd57dfd1ba31ecd77
Daniel Townsend <dan@dantownsend.co.uk>
2023-07-24 21:09:24 +0100

    catch deletion errors in `RowListing` page

--- admin_ui/src/components/EditRowForm.vue
+++ admin_ui/src/components/EditRowForm.vue
@@ -132,7 +132,24 @@ export default Vue.extend({
                     tableName: this.tableName,
                     rowID: this.rowID
                 }
-                await this.$store.dispatch("deleteRow", config)
+
+                try {
+                    await this.$store.dispatch("deleteRow", config)
+                } catch (error) {
+                    this.errors = parseErrorResponse(
+                        error.response.data,
+                        error.response.status
+                    )
+
+                    var message: APIResponseMessage = {
+                        contents: "Unable to delete the row.",
+                        type: "error"
+                    }
+                    this.$store.commit("updateApiResponseMessage", message)
+
+                    return
+                }
+
                 alert("Successfully deleted row")
                 this.$router.push({
                     name: "rowListing",
