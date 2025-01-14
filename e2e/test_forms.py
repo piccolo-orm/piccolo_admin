05b5cf032c77970f7e2eeb7afd61e9d94559a941
Daniel Townsend <dan@dantownsend.co.uk>
2025-01-14 13:08:15 +0000

    add a playwright test

--- e2e/test_forms.py
+++ e2e/test_forms.py
@@ -2,7 +2,9 @@ from playwright.sync_api import Page
 
 from piccolo_admin.example.forms.csv import FORM as CSV_FORM
 from piccolo_admin.example.forms.image import FORM as IMAGE_FORM
+from piccolo_admin.example.forms.nullable import FORM as NULLABLE_FORM
 
+from .conftest import BASE_URL
 from .pages import FormPage, LoginPage
 
 
@@ -54,3 +56,26 @@ def test_image_form(page: Page, dev_server):
 
     download = download_info.value
     assert download.suggested_filename == "movie_listings.jpg"
+
+
+def test_nullable_form(page: Page, dev_server):
+    """
+    Make sure a form with nullable fields can be submitted successfully.
+    """
+    login_page = LoginPage(page=page)
+    login_page.reset()
+    login_page.login()
+
+    form_page = FormPage(
+        page=page,
+        form_slug=NULLABLE_FORM.slug,
+    )
+    form_page.reset()
+
+    with page.expect_response(
+        lambda response: response.url
+        == f"{BASE_URL}/api/forms/nullable-fields/"
+        and response.request.method == "POST"
+        and response.status == 200
+    ):
+        form_page.submit_form()
