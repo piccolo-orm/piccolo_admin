8fa977af37c18c4088ffa9c1322fc8fd031ab870
Daniel Townsend <dan@dantownsend.co.uk>
2025-04-09 18:30:47 +0100

    fix warnings with process.env

--- admin_ui/src/translations.ts
+++ admin_ui/src/translations.ts
@@ -2,7 +2,7 @@ import { createI18n } from "vue-i18n"
 
 const i18n = createI18n({
     locale: "en",
-    silentTranslationWarn: process.env.NODE_ENV === "production"
+    silentTranslationWarn: import.meta.env.PROD
 })
 
 export default i18n
