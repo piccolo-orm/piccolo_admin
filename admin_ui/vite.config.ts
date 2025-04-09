7eefc137dc47e5dd447cbe7755f95cb2f097dbc6
Daniel Townsend <dan@dantownsend.co.uk>
2025-04-09 16:30:35 +0100

    use latest dependencies

--- admin_ui/vite.config.ts
+++ admin_ui/vite.config.ts
@@ -1,11 +1,12 @@
-import { fileURLToPath, URL } from "url"
+import { fileURLToPath, URL } from "node:url"
 
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import vueDevTools from "vite-plugin-vue-devtools"
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [vue(), vueDevTools()],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url))
