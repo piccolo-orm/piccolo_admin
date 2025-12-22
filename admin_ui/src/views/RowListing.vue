d98de7d9a4bd95bfa2bfb32ee919300ee4a59851
abhishek-compro <abhishek.prasad@noorahealth.org>
2025-12-22 20:04:29 +0530

    Allow horizontal scroll on listing page without moving action buttons out of sight

--- admin_ui/src/views/RowListing.vue
+++ admin_ui/src/views/RowListing.vue
@@ -866,6 +866,7 @@ div.wrapper {
 
         div.table_wrapper {
             position: relative;
+            overflow: auto;
 
             p#loading_indicator {
                 position: absolute;
